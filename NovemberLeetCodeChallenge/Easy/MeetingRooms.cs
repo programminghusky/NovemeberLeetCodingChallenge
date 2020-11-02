61fa661e740dbead2cc47c7fd3e32de03d81fd90
programminghusky <husky2020coding@gmail.com>
2020-11-02 14:28:36 -0800

    Determine if a person can attend all meetings

---
+++ NovemberLeetCodeChallenge/Easy/MeetingRooms.cs
@@ -0,0 +1,31 @@
+﻿/* Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovemberLeetCodeChallenge.Easy
+{
+    class MeetingRooms
+    {
+        public bool CanAttendMeetings(int[][] intervals)
+        {
+            Array.Sort(intervals, new ArrayComparer());
+            for (int i = 0; i < intervals.Length - 1; i++)
+            {
+                if (intervals[i][1] > intervals[i + 1][0])
+                    return false;
+            }
+            return true;
+        }
+
+        class ArrayComparer : IComparer<int[]>
+        {
+            public int Compare(int[] x, int[] y)
+            {
+                return x[0] - y[0];
+            }
+        }
+    }
+}
