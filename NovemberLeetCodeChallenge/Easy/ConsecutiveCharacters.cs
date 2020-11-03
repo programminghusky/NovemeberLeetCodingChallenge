05479737aa2327b7204f4a441e2ccc63f21fe9c2
programminghusky <husky2020coding@gmail.com>
2020-11-03 08:12:39 -0800

    Consecutive unique character

---
+++ NovemberLeetCodeChallenge/Easy/ConsecutiveCharacters.cs
@@ -0,0 +1,34 @@
+﻿/* Given a string s, the power of the string is the maximum length of a non-empty substring that contains only one unique character.
+ * Return the power of the string.  */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovemberLeetCodeChallenge.Easy
+{
+    class ConsecutiveCharacters
+    {
+        public int MaxPower(string s)
+        {
+            if (s.Length < 2)
+                return s.Length;
+            int maxLength = 0, count = 0;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i - 1] == s[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 0;
+                }
+
+                maxLength = Math.Max(count + 1, maxLength);
+            }
+            return maxLength;
+        }
+    }
+}
---
+++ NovemberLeetCodeChallenge/Medium/InsertionSortList.cs
@@ -0,0 +1,14 @@
+﻿/* Sort a linked list using insertion sort.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovemberLeetCodeChallenge.Medium
+{
+    class InsertionSortList
+    {
+
+    }
+}
