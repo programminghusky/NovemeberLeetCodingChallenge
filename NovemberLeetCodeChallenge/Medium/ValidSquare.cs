12654b6e26d4f09ac2adacf7ce2389ea7a37b2e3
programminghusky <husky2020coding@gmail.com>
2020-11-11 13:10:22 -0800

    Check if the given coordinates of 4 points form a valid square using all permutations

--- NovemberLeetCodeChallenge/Medium/ValidSquare.cs
+++ NovemberLeetCodeChallenge/Medium/ValidSquare.cs
@@ -12,9 +12,34 @@ namespace NovemberLeetCodeChallenge.Medium
     {
         public bool ValidSquare1(int[] p1, int[] p2, int[] p3, int[] p4)
         {
-            return Check(p1, p2, p3, p4) || Check(p1, p2, p4, p3) || Check(p1, p3, p2, p4);
+            //return Check(p1, p2, p3, p4) || Check(p1, p2, p4, p3) || Check(p1, p3, p2, p4);
+            int[][] p = { p1, p2, p3, p4 };
+            return CheckAllPermutations(p, 0);
         }
 
+        bool CheckAllPermutations(int[][]p, int l)
+        {
+            if (l == 4)
+                return Check(p[0], p[1], p[2], p[3]);
+            else
+            {
+                bool res = false;
+                for (int i = l; i < 4; i++)
+                {
+                    Swap(p, l, i);
+                    res |= CheckAllPermutations(p, l + 1);
+                    Swap(p, l, i);
+                }
+                return res;
+            }
+        }
+
+        public void Swap(int[][]p, int x, int y)
+        {
+            int[] temp = p[x];
+            p[x] = p[y];
+            p[y] = temp;
+        }
         public bool Check(int[] p1, int[] p2, int[] p3, int[] p4)
         {
             return (Distance(p1,p2) > 0  && Distance(p1, p2) == Distance (p2,p3) &&
